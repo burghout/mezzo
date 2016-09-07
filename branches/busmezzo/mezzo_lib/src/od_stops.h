b07b9bdbea2c19b4f01c0db938a7fbfe0792192d
Trateotu <DYLeffler@gmail.com>
2016-09-07 18:40:37 +0200

    Updates to passenger decision writers
    - Changes to match I/O doc
    - Formatting changes

--- branches/busmezzo/mezzo_lib/src/od_stops.h
+++ branches/busmezzo/mezzo_lib/src/od_stops.h
@@ -21,15 +21,53 @@ typedef vector <Passenger*> passengers;
 class Pass_boarding_decision // container object holding output data for passenger boarding decision
 {
 public:
-	Pass_boarding_decision (int pass_id_, int original_origin_, int destination_stop_, int line_id_, int trip_id_, int stop_id_, double time_,	double generation_time_,
-							double boarding_probability_, bool boarding_,double u_boarding_, double u_staying_):
-							pass_id(pass_id_),original_origin(original_origin_),destination_stop(destination_stop_),line_id(line_id_),trip_id(trip_id_), stop_id(stop_id_),time(time_),generation_time(generation_time_),boarding_probability(boarding_probability_),boarding(boarding_),
-							u_boarding(u_boarding_),u_staying(u_staying_) {}
+	Pass_boarding_decision(
+		int pass_id_,
+		int original_origin_,
+		int destination_stop_,
+		int line_id_,
+		int trip_id_,
+		int stop_id_,
+		double time_,
+		double generation_time_,
+		double boarding_probability_,
+		bool boarding_,
+		double u_boarding_,
+		double u_staying_
+		): pass_id(pass_id_),original_origin(original_origin_),destination_stop(destination_stop_),line_id(line_id_),trip_id(trip_id_), stop_id(stop_id_),time(time_),generation_time(generation_time_),boarding_probability(boarding_probability_),boarding(boarding_),u_boarding(u_boarding_),u_staying(u_staying_){}
+
 	virtual ~Pass_boarding_decision(); //!< destructor
-	void write (ostream& out) { out << pass_id << '\t' << original_origin << '\t' << destination_stop << '\t' << line_id << '\t'<< trip_id << '\t'<< stop_id<< '\t'<< time << '\t'<< generation_time << '\t' << boarding_probability << '\t'
-		<< boarding << '\t'<< u_boarding << '\t'<< u_staying <<'\t'<< endl; }
-	void reset () { pass_id = 0; original_origin = 0; destination_stop = 0; line_id = 0; trip_id = 0; stop_id = 0; time = 0;
-	generation_time = 0; boarding = 0; u_boarding = 0; u_staying = 0; }
+
+	void write(ostream& out){
+		out << pass_id << '\t'
+			<< original_origin << '\t'
+			<< destination_stop << '\t'
+			<< line_id << '\t'
+			<< trip_id << '\t'
+			<< stop_id<< '\t'
+			<< time << '\t'
+			<< generation_time << '\t'
+			<< boarding_probability << '\t'
+			<< boarding << '\t'
+			<< u_boarding << '\t'
+			<< u_staying <<'\t'
+			<< endl;
+	}
+
+	void reset(){
+		pass_id = 0; 
+		original_origin = 0;
+		destination_stop = 0;
+		line_id = 0;
+		trip_id = 0;
+		stop_id = 0;
+		time = 0;
+		generation_time = 0;
+		boarding = 0;
+		u_boarding = 0;
+		u_staying = 0;
+	}
+
 	int pass_id;
 	int original_origin;
 	int destination_stop;
@@ -47,13 +85,34 @@ public:
 class Pass_alighting_decision // container object holding output data for passenger alighting decision
 {
 public:
-	Pass_alighting_decision (int pass_id_, int original_origin_, int destination_stop_, int line_id_, int trip_id_, int stop_id_, double time_,	double generation_time_, int chosen_alighting_stop_,
-							map<Busstop*,pair<double,double>> alighting_MNL_):
-							pass_id(pass_id_),original_origin(original_origin_),destination_stop(destination_stop_),line_id(line_id_),trip_id(trip_id_), stop_id(stop_id_),time(time_),generation_time(generation_time_),chosen_alighting_stop(chosen_alighting_stop_),alighting_MNL(alighting_MNL_) {}
+	Pass_alighting_decision(
+		int pass_id_,
+		int original_origin_,
+		int destination_stop_,
+		int line_id_,
+		int trip_id_,
+		int stop_id_,
+		double time_,
+		double generation_time_,
+		int chosen_alighting_stop_,
+		map<Busstop*,pair<double,double>> alighting_MNL_
+	): pass_id(pass_id_),original_origin(original_origin_),destination_stop(destination_stop_),line_id(line_id_),trip_id(trip_id_), stop_id(stop_id_),time(time_),generation_time(generation_time_),chosen_alighting_stop(chosen_alighting_stop_),alighting_MNL(alighting_MNL_){}
+
 	virtual ~Pass_alighting_decision(); //!< destructor
-	void write (ostream& out);
-	void reset () { pass_id = 0; original_origin = 0; destination_stop = 0; line_id = 0; trip_id = 0; stop_id = 0; time = 0;
-	generation_time = 0; }
+	
+	void write(ostream& out);
+	
+	void reset(){
+		pass_id = 0;
+		original_origin = 0;
+		destination_stop = 0;
+		line_id = 0;
+		trip_id = 0;
+		stop_id = 0;
+		time = 0;
+		generation_time = 0;
+	}
+	
 	int pass_id;
 	int original_origin;
 	int destination_stop;
@@ -69,12 +128,30 @@ public:
 class Pass_connection_decision // container object holding output data for passenger connection decision
 {
 public:
-	Pass_connection_decision (int pass_id_, int original_origin_, int destination_stop_, int stop_id_, double time_, double generation_time_, int chosen_connection_stop_, map<Busstop*,pair<double,double>> connecting_MNL_):
-							pass_id(pass_id_), original_origin(original_origin_), destination_stop(destination_stop_), stop_id(stop_id_),time(time_), generation_time(generation_time_), chosen_connection_stop(chosen_connection_stop_), connecting_MNL(connecting_MNL_) {}
+	Pass_connection_decision(
+		int pass_id_, 
+		int original_origin_, 
+		int destination_stop_, 
+		int stop_id_, 
+		double time_, 
+		double generation_time_, 
+		int chosen_connection_stop_, 
+		map<Busstop*,pair<double,double>> connecting_MNL_
+	):	pass_id(pass_id_), original_origin(original_origin_), destination_stop(destination_stop_), stop_id(stop_id_),time(time_), generation_time(generation_time_), chosen_connection_stop(chosen_connection_stop_), connecting_MNL(connecting_MNL_) {}
+	
 	virtual ~Pass_connection_decision(); //!< destructor
+	
 	void write (ostream& out);
-	void reset () { pass_id = 0; original_origin = 0; destination_stop = 0; stop_id = 0; time = 0;
-	generation_time = 0; }
+	
+	void reset(){
+		pass_id = 0; 
+		original_origin = 0; 
+		destination_stop = 0; 
+		stop_id = 0; 
+		time = 0;
+		generation_time = 0;
+	}
+	
 	int pass_id;
 	int original_origin;
 	int destination_stop;
@@ -88,22 +165,64 @@ public:
 class Pass_waiting_experience // container object holding output data for passenger waiting experience
 {
 public:
-	Pass_waiting_experience (int pass_id_, int original_origin_, int destination_stop_, int line_id_, int trip_id_, int stop_id_, double time_,	double generation_time_,
-							double expected_WT_PK_, bool RTI_level_available_, double projected_WT_RTI_, double experienced_WT_, double AWT_, int nr_missed_):
-							pass_id(pass_id_),original_origin(original_origin_),destination_stop(destination_stop_),line_id(line_id_),trip_id(trip_id_), stop_id(stop_id_),time(time_),generation_time(generation_time_),expected_WT_PK(expected_WT_PK_),RTI_level_available(RTI_level_available_),projected_WT_RTI(projected_WT_RTI_),
-							experienced_WT(experienced_WT_),AWT(AWT_),nr_missed(nr_missed_) {}
+	Pass_waiting_experience(
+		int pass_id_,
+		int original_origin_,
+		int destination_stop_,
+		int line_id_,
+		int trip_id_, 
+		int stop_id_, 
+		double time_, 
+		double generation_time_,
+		double expected_WT_PK_, 
+		bool RTI_level_available_, 
+		double projected_WT_RTI_, 
+		double experienced_WT_, 
+		double AWT_, 
+		int nr_missed_
+	): pass_id(pass_id_),original_origin(original_origin_),destination_stop(destination_stop_),line_id(line_id_),trip_id(trip_id_), stop_id(stop_id_),time(time_),generation_time(generation_time_),expected_WT_PK(expected_WT_PK_),RTI_level_available(RTI_level_available_),projected_WT_RTI(projected_WT_RTI_),experienced_WT(experienced_WT_),AWT(AWT_),nr_missed(nr_missed_) {}
 	virtual ~Pass_waiting_experience(); //!< destructor
-	void write (ostream& out) { out << pass_id << '\t' << original_origin << '\t' << destination_stop << '\t' << line_id << '\t'<< trip_id << '\t'<< stop_id<< '\t'<< time << '\t'<< generation_time << '\t' << expected_WT_PK << '\t'
-		<< RTI_level_available << '\t'<< projected_WT_RTI << '\t'<< experienced_WT <<'\t'<< AWT <<'\t'<< nr_missed <<'\t'<< endl; }
-	void reset () { pass_id = 0; original_origin = 0; destination_stop = 0; line_id = 0; trip_id = 0; stop_id = 0; time = 0;
-	generation_time = 0; expected_WT_PK = 0; RTI_level_available = 0; projected_WT_RTI = 0; experienced_WT = 0; AWT = 0;}
+	
+	void write(ostream& out){ 
+		out << pass_id << '\t' 
+			<< original_origin << '\t' 
+			<< destination_stop << '\t' 
+			<< line_id << '\t'
+			<< trip_id << '\t'
+			<< stop_id<< '\t'
+			<< time << '\t'
+			<< generation_time << '\t' 
+			<< expected_WT_PK << '\t'
+			<< RTI_level_available << '\t'
+			<< projected_WT_RTI << '\t'
+			<< experienced_WT <<'\t'
+			<< AWT <<'\t'
+			<< nr_missed <<'\t'
+			<< endl;
+	}
+	
+	void reset(){
+		pass_id = 0; 
+		original_origin = 0; 
+		destination_stop = 0; 
+		line_id = 0; 
+		trip_id = 0; 
+		stop_id = 0; 
+		time = 0;
+		generation_time = 0; 
+		expected_WT_PK = 0; 
+		RTI_level_available = 0; 
+		projected_WT_RTI = 0; 
+		experienced_WT = 0; 
+		AWT = 0;
+	}
+	
 	int pass_id;
 	int original_origin;
 	int destination_stop;
 	int line_id;
 	int trip_id;
 	int stop_id;
-	int nr_missed; //Nr of missed buses due to overcrowding
 	double time;
 	double generation_time;
 	double expected_WT_PK;
@@ -111,15 +230,38 @@ public:
 	double projected_WT_RTI;
 	double experienced_WT;
 	double AWT;
+	int nr_missed; //Nr of missed buses due to overcrowding
 };
 
 class Pass_onboard_experience
 {
 public:
-	Pass_onboard_experience (int pass_id_, int original_origin_, int destination_stop_, int line_id_, int trip_id_, int stop_id_, int leg_id_, double expected_ivt_, pair<double, double> experienced_ivt_):
-							pass_id(pass_id_),original_origin(original_origin_),destination_stop(destination_stop_),line_id(line_id_), trip_id(trip_id_), stop_id(stop_id_), 
-							leg_id(leg_id_), expected_ivt(expected_ivt_), experienced_ivt(experienced_ivt_) {}
-							void write (ostream& out) { out << pass_id << '\t' << original_origin << '\t' << destination_stop << '\t' << line_id << '\t'<< trip_id << '\t' << stop_id << '\t' << leg_id << '\t' << expected_ivt << '\t' << experienced_ivt.first << '\t' << experienced_ivt.second << endl;}
+	Pass_onboard_experience(
+		int pass_id_, 
+		int original_origin_, 
+		int destination_stop_, 
+		int line_id_, 
+		int trip_id_, 
+		int stop_id_, 
+		int leg_id_, 
+		double expected_ivt_, 
+		pair<double, double> experienced_ivt_
+	): pass_id(pass_id_),original_origin(original_origin_),destination_stop(destination_stop_),line_id(line_id_), trip_id(trip_id_), stop_id(stop_id_), leg_id(leg_id_), expected_ivt(expected_ivt_), experienced_ivt(experienced_ivt_) {}
+	
+	void write (ostream& out){
+		out << pass_id << '\t' 
+			<< original_origin << '\t' 
+			<< destination_stop << '\t' 
+			<< line_id << '\t'
+			<< trip_id << '\t' 
+			<< stop_id << '\t' 
+			<< leg_id << '\t' 
+			<< expected_ivt << '\t' 
+			<< experienced_ivt.first << '\t' 
+			<< experienced_ivt.second 
+			<< endl;
+	}
+	
 	int pass_id;
 	int original_origin;
 	int destination_stop;
