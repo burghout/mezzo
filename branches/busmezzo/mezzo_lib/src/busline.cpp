9a1927bb62724ce2fc2db9b8d0b4c0c4dfb483e1
Trateotu <DYLeffler@gmail.com>
2017-03-23 11:56:08 +0100

    changed Change_arrival_rate::execute to match Action::execute
    - Currently does not seem to be used anywhere
    - Only reference to the Change_arrival_rate class i can find is for
    demand format 10 in Network::read_passenger_rates_format1_TD_slices

--- branches/busmezzo/mezzo_lib/src/busline.cpp
+++ branches/busmezzo/mezzo_lib/src/busline.cpp
@@ -2460,7 +2460,7 @@ void Change_arrival_rate::book_update_arrival_rates (Eventlist* eventlist, doubl
 	eventlist->add_event(time,this);
 }
 
-bool Change_arrival_rate::execute()
+bool Change_arrival_rate::execute(Eventlist* eventlist, double time)
 {		
 	for (TD_demand::iterator stop_iter = arrival_rates_TD.begin(); stop_iter != arrival_rates_TD.end(); stop_iter++)
 	{
