e4a366c8ba3172480c0444be909e5fc09fd92842
Trateotu <DYLeffler@gmail.com>
2018-04-04 10:48:05 +0200

    added Request data structure with compare by time function

--- branches/busmezzo/mezzo_lib/src/controlcenter.h
+++ branches/busmezzo/mezzo_lib/src/controlcenter.h
@@ -12,6 +12,10 @@ General Notes:
 - TripGenerator
 - TripMatcher
 - FleetScheduler
+
+Registers (unregisters) service vehicles
+Offers interface to connected vehicles as well as connected passengers
+
 */
 
 //#include "busline.h"
@@ -25,6 +29,54 @@ using namespace std;
 //struct RequestQueue
 //struct TripQueue
 
+//structure that corresponds to a request from a passenger for a vehicle to travel between an origin stop and a destination stop
+struct Request
+{
+	int pass_id;		//id of passenger that created request
+	int ostop_id;		//id of origin stop
+	int dstop_id;		//id of destination stop
+	int load;			//number of passengers in request
+	double time;		//time request was generated
+
+	Request(int pid, int oid, int did, int l, double t) : pass_id(pid), ostop_id(oid), dstop_id(did), load(l), time(t) {}
+
+	bool operator == (const Request& rhs) const
+	{
+		return (pass_id == rhs.pass_id && ostop_id == rhs.ostop_id && dstop_id == rhs.dstop_id && load == rhs.load && time == rhs.time);
+	}
+
+	bool operator < (const Request& rhs) const  //default comparator sorts by order of attributes
+	{
+		if (pass_id != rhs.pass_id)
+			return pass_id < rhs.pass_id;
+		else if (ostop_id != rhs.ostop_id)
+			return ostop_id < rhs.ostop_id;
+		else if (dstop_id != rhs.dstop_id)
+			return dstop_id < rhs.dstop_id;
+		else if (load != rhs.load)
+			return load < rhs.load;
+		else
+			return time < rhs.time;
+	}
+};
+
+//compare in the order of smallest time, smallest load, smallest origin stop id, smallest destination stop id and finally smallest passenger id
+struct compareRequestByTime
+{
+	inline bool operator() (const Request& req1, const Request& req2)
+	{
+		if (req1.time != req2.time)
+			return req1.time < req2.time;
+		else if (req1.load != req2.load)
+			return req1.load < req2.load;
+		else if (req1.ostop_id != req2.ostop_id)
+			return req1.ostop_id < req2.ostop_id;
+		else if (req1.dstop_id != req2.dstop_id)
+			return req1.dstop_id < req2.dstop_id;
+		else
+			return req1.pass_id < req2.pass_id;
+	}
+};
 
 /*Responsible for generating requests and adding these to RequestQueue*/
 class RequestHandler
@@ -122,6 +174,5 @@ private:
 	TripMatcher tm_;
 };
 
-extern ControlCenter* CC;
 
 #endif
