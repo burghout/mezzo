7c81ccb14febe8a8b6f511f12e3ab2e44fc2333a
Trateotu <DYLeffler@gmail.com>
2018-05-31 15:22:47 +0200

    drt vehicles do not have any trip associated with them between resets
    - vehicles that run fixed schedule trips from input files do not
    currently reset their curr_trip

--- branches/busmezzo/mezzo_lib/src/network.cpp
+++ branches/busmezzo/mezzo_lib/src/network.cpp
@@ -7820,6 +7820,7 @@ bool Network::init()
 			double init_time = get<2>(drt_init);
 			vector<int> sroute_ids = get<3>(drt_init);
 
+			bus->set_curr_trip(nullptr); //unlike non-dynamically generated bus/trips this bus should not have a trip between resets as well
 			ccmap[1]->connectVehicle(bus); //connect vehicle to a control center
 			stop->add_unassigned_bus_arrival(bus, init_time); //should be in a Null state until their init_time
 			eventlist->add_event(init_time, stop);	//add a Busstop event scheduled for the init_time of vehicle  to switch state of bus to IdleEmpty from Null
