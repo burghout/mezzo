1033c83c1bf3a2574f8b6b9755850035f386bf35
Wilco Burghout <burghout@yahoo.co.uk>
2018-09-27 15:55:09 +0200

    added tests for time-dependent graphs

--- branches/busmezzo/tests/routestest/test_routes.cpp
+++ branches/busmezzo/tests/routestest/test_routes.cpp
@@ -1,6 +1,7 @@
 #include <QString>
 #include <QtTest/QtTest>
 #include "Graph.h"
+#include "linktimes.h"
 #ifdef Q_OS_WIN
 #include <direct.h>
 #define chdir _chdir
@@ -46,44 +47,79 @@ public:
 
 
 private Q_SLOTS:
-    void testInitGraph();
+    void testInitGraphs();
     void testSimpleGraph();
+    void testTimeDependentGraph();
 
 private:
     //    NetworkThread* nt; //!< contains the network thread
     //    Network* net;
 
-    Graph<double, GraphNoInfo<double> > * simpleGraph;
+    Graph<double, GraphNoInfo<double> > * simpleGraph; // simple graph with fixed costs
+    Graph<double, LinkTimeInfo > * graph; // graph with time-dependent costs in LinkTimeInfo
+    LinkTimeInfo* linkinfo; // time-dependent link costs
 
 
 };
 
-void TestRoutes::testInitGraph()
+void TestRoutes::testInitGraphs()
 {   
     simpleGraph = new Graph<double, GraphNoInfo<double> > (10, 10, 9999999.0); // creates a simple graph with fixed link costs <double>
     simpleGraph->addLink(1,1,2,10.0); // link 1 from node 1 to node 2
-    simpleGraph->addLink(2,2,1,10.0); // link 2 from node 2 to node 1
-    simpleGraph->addLink(3,2,3,10.0); // link 2 from node 2 to node 1
-    simpleGraph->addLink(4,3,2,10.0); // link 2 from node 2 to node 1
-    simpleGraph->addLink(5,3,4,10.0); // link 2 from node 2 to node 1
+    simpleGraph->addLink(2,2,1,10.0);
+    simpleGraph->addLink(3,2,3,10.0);
+    simpleGraph->addLink(4,3,2,10.0);
+    simpleGraph->addLink(5,3,4,10.0);
     QVERIFY (simpleGraph->nNodes()==10);
     QVERIFY (simpleGraph->nLinks()==10);
-}
 
+    // Time dependent graph init
+    // 1. set up the linkinfo object that contains the time dependent costs for each link and time period
+    linkinfo=new LinkTimeInfo();
+
+    for (int id=1; id<=5; ++id)
+    {
+        LinkTime* lt = new LinkTime(id,3,20.0); // 3 periods of 20.0 seconds each (totaltime = 60.0)
+        lt->times [0] = 10.0;
+        lt->times [1] = 20.0;
+        lt->times [2] = 30.0; // increasing travel times for all links, from 10.0 to 30.0
+        linkinfo->times.insert(pair <int,LinkTime*>(id,lt));
+    }
+
+    graph = new Graph <double, LinkTimeInfo> (10,10,9999999.0);
+    graph->addLink(1,1,2,10.0); // link 1 from node 1 to node 2
+    graph->addLink(2,2,1,10.0);
+    graph->addLink(3,2,3,10.0);
+    graph->addLink(4,3,2,10.0);
+    graph->addLink(5,3,4,10.0);
+    graph->set_downlink_indices();
+
+    QVERIFY (graph->nNodes()==10);
+    QVERIFY (graph->nLinks()==10);
+}
 void TestRoutes::testSimpleGraph()
 {
     for (int i = 1; i<=4; ++i) // for all root links except link 5
     {
         simpleGraph->labelCorrecting(i); // find shortest paths using label correcting from root link
+        simpleGraph->printNodePathTree();
         for (int j=1; j <= 4; ++j) // for all destinations
         {
-  //          qDebug() << i << " to " << j;
-             QVERIFY(simpleGraph->reachable(j)); // test if they are reachable
+            QVERIFY(simpleGraph->reachable(j)); // test if they are reachable
         }
     }
 
 }
 
+void TestRoutes::testTimeDependentGraph()
+{
+    graph->labelCorrecting(1,0.0,linkinfo);
+    graph->printNodePathTree();
+    graph->labelCorrecting(1,20.0,linkinfo);
+    graph->printNodePathTree();
+    graph->labelCorrecting(1,40.0,linkinfo);
+    graph->printNodePathTree();
+}
 
 QTEST_APPLESS_MAIN(TestRoutes)
 
