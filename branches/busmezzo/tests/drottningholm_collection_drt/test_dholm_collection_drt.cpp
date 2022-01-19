5594c0188f4ec994f69699a2226481037c74784f
trateotu <dyleffler@gmail.com>
2022-01-19 15:41:59 +0100

    missing from previous commit

--- branches/busmezzo/tests/drottningholm_collection_drt/test_dholm_collection_drt.cpp
+++ branches/busmezzo/tests/drottningholm_collection_drt/test_dholm_collection_drt.cpp
@@ -154,7 +154,9 @@ void TestDrottningholmCollection_drt::testInitNetwork()
     qDebug() << "Removing file " + path_set_generation_filename + ": " << QFile::remove(path_set_generation_filename); //remove old passenger path sets
     qDebug() << "Initializing network in " + QString::fromStdString(network_path_1);
 
-    ::fwf_wip::csgm_no_merging_or_filtering_paths = false; //set manually (default false)
+    ::fwf_wip::csgm_no_merging_rules = false; //set manually (default false)
+    ::fwf_wip::csgm_no_filtering_dominancy_rules = false; //set manually (default false)
+    
     ::PARTC::drottningholm_case = true; //set manually (default false)
     
     nt->init();
